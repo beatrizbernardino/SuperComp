c3af57655e360f1b3a25d4b11a3bc7a366a58e10
supercomp_gpu <pbeatriz18@hotamil.com.br>
2022-06-03 21:12:48 +0000

    versao ruim

--- Exaustiva_GPU/exaustiva.cu
+++ Exaustiva_GPU/exaustiva.cu
@@ -13,58 +13,87 @@
 #include <random>
 using namespace std;
 
-struct meu_functor
-{
-    string * subseq_a;
-    string * subseq_b;
-    thrust::device_ptr<int> sa;
-    thrust::device_ptr<int> sb;
-    thrust::device_ptr<char> a;
 
-    // subseq_a(subseq_a_),subseq_b(subseq_b_),
-    // vector<string>subseq_a_, vector<string>subseq_b_, 
+struct subs{
 
-    meu_functor(string *subseq_a_, string *subseq_b_,thrust::device_ptr<int> sa_,thrust::device_ptr<int> sb_ ,thrust::device_ptr<char> a_ ): 
-    subseq_a(subseq_a_),subseq_b(subseq_b_),sa(sa_), sb(sb_), a(a_){};
-    __host__ __device__
-    int operator()(const int& x) {
-       
-        // thrust::device_ptr<char> a(sa[x][0].begin(), sa[x][0].end());
-        // for(int i=0; i<int(subseq_a[sa[x]].length()); i++){
-        //     a[i]='a';
-
-        // }
-        // for (int i=0; i< int(subseq_b.size()); i++){
-        //     string b= subseq_b[x][i];
-
-        // }
-
-       
-        return 2;
-
-        // return 2*2;
-
-        // int index=sa[x];
-        // string a= subseq_a[index];
-        // for (int i =0; i<int(&sb.size()); i++){
-        //     string b= subseq_b[i];
-        //     // thrust::transform(c0, c1, calc[1].begin()+1, meu_functor(subseq_a, subseq_b, sa, sb));   
-        //     // thrust::inclusive_scan(calc[1].begin()+1, calc[1].end(), calc[0].begin()+1, thrust::maximum<int>());
-        // }
-
-    }
+    int idx_seq;
+    int tam_seq;
 };
 
 
-// struct meu_functor
-// {
-//     int a;    
-//     meu_functor(int a_) : a(a_) {};
-//     __host__ __device__
-//     double operator()(const int& x, const int& y) {
-//            return a * x + y;
-//     }
-// };
+struct calc_b
+{
+    thrust::device_ptr<subs> index_seqb;
+    thrust::device_ptr<char> sequencias_b;
+    thrust::device_ptr<char> a;
+    
+    calc_b(thrust::device_ptr<subs> index_seqb_,  thrust::device_ptr<char> sequencias_b_,thrust::device_ptr<char> a_ ): 
+    index_seqb(index_seqb_), sequencias_b(sequencias_b_), a(a_) {};
+
+    __host__ __device__
+    int operator()(const int& x) {
+
+        return 2;
+    }
+};
+
+struct meu_functor
+{
+
+    
+    thrust::device_ptr<subs> index_seqa;
+    thrust::device_ptr<subs> index_seqb;
+    thrust::device_ptr<char> sequencias_a;
+    thrust::device_ptr<char> sequencias_b;
+    int size_b;
+
+
+    meu_functor(thrust::device_ptr<subs> index_seqa_,thrust::device_ptr<subs> index_seqb_, thrust::device_ptr<char> sequencias_a_, thrust::device_ptr<char> sequencias_b_, int size_b_): 
+    index_seqa(index_seqa_),index_seqb(index_seqb_), sequencias_a(sequencias_a_) ,sequencias_b(sequencias_b_), size_b(size_b_) {};
+    __host__ __device__
+    int operator()(const int& x) {
+
+        thrust::device_ptr<char> a;
+        subs seqa_info= index_seqa[x];
+
+        int w=0;
+        for(int i = seqa_info.idx_seq; i< (seqa_info.idx_seq+ seqa_info.tam_seq); i++){
+
+            a[w]=sequencias_a[i];
+            w++;
+
+        }
+
+        for(int i = 0; i< size_b; i++){
+
+            thrust::device_ptr<char> b;
+            subs seqa_info= index_seqb[i];
+
+            for(int k = seqb_info.idx_seq; k< (seqb_info.idx_seq+ seqb_info.tam_seq); k++){
+
+                sequencias_b[i];
+                
+
+            }
+
+        }
+
+
+        
+
+        // thrust::counting_iterator<int> c0(0);
+        // thrust::counting_iterator<int> c1(size_b);
+        // thrust::device_ptr<int> scores;
+
+    
+        // thrust::transform(thrust::device, c0, c1, scores, calc_b(index_seqb, a, sequencias_b));
+
+
+
+        return 0;
+        
+    }
+};
 
 
 int main()
@@ -93,7 +122,8 @@ int main()
         cin>> b[i];
         
     }
-
+   
+   
 
     for(int i=0; i< N; i++){
         string s="";
@@ -101,7 +131,10 @@ int main()
 
             s+=a[j];
             subseq_a.push_back(s);
+          
         }
+
+
     }
 
     int suba_size= subseq_a.size();
@@ -117,78 +150,86 @@ int main()
 
     int subb_size= subseq_b.size();
 
+    thrust::device_vector<char> sequencias_a;
+    thrust::device_vector<char> sequencias_b;
 
-    thrust::device_vector<int> sa;
-   
-    thrust::device_vector<int> sb(subb_size);
-
-    thrust::sequence( sb.begin(), sb.end(), 1);
+    thrust::device_vector<subs> index_seqa;
+    thrust::device_vector<subs> index_seqb;
 
 
-        for(int i=0; i<suba_size; i++){
+    int idx=0;
+    for( int i=0; i <int(subseq_a.size()); i++){
 
-            
-            for(int j=0; j<int(subb_size); j++){
+            subs seqa;
 
-                sa.push_back(i);  
-  
+            for(int c=0; c<int(subseq_a[i].size()); c++){
+
+                sequencias_a.push_back(subseq_a[i][c]);
+
+
             }
-        }
 
-        for(int i=0; i<subb_size; i++){
+            seqa.idx_seq=idx;
+            seqa.tam_seq=(int(subseq_a[i].size()));
+            index_seqa.push_back(seqa);
+            idx+=int(subseq_a[i].size());
+    }
+
+    idx=0;
+    for( int i=0; i <int(subseq_b.size()); i++){
+
+            subs seqb;
+
+            for(int c=0; c<int(subseq_b[i].size()); c++){
+
+                sequencias_b.push_back(subseq_b[i][c]);
+
+            }
+
+            seqb.idx_seq=idx;
+            seqb.tam_seq=(int(subseq_b[i].size()));
+            idx+=subseq_b[i].size();
+            index_seqb.push_back(seqb);
+    }
+
+    // for(int h=0; h<int(index_seqa.size() ); h++){
+    //     subs seq;
+    //     seq=index_seqa[h];
+    //     cout<<seq.idx_seq<<endl;
+    // }
+
+    thrust::counting_iterator<int> c0(1);
+    thrust::counting_iterator<int> c1(int(index_seqa.size()+1));
+    thrust::device_vector<int> max_scores(int(index_seqa.size()) *int(index_seqb.size()) );
+
+    int size_b=  int(index_seqb.size());
 
 
-            sa.push_back(i);  
-  
-        }
+
+    thrust::transform(c0, c1, max_scores.begin()+1, meu_functor( index_seqa.data(),index_seqb.data(), sequencias_a.data(), sequencias_b.data(), size_b));
+    
+
+    // for (int g=0; g< int(max_scores.size()); g++){
+    //     cout<<max_scores[g]<<endl;
+    // }
+    // int  best_score= thrust::reduce(max_scores.begin(), max_scores.end(), 0, thrust::maximum<int>());
+
+    // cout<<best_score<<endl;
+
+
+// [T,T,G,T,G,C,G,G,C,C]
+// [T,T,G,T,G,C,G,G,C,C]
+
+
+
+    
+
+
+
+
         
- 
-        
-
-        thrust::device_vector<int> calc[2];
-        calc[0].resize(N+1);
-        calc[1].resize(N+1); 
-        thrust::fill(calc[0].begin(), calc[0].end(),0);
-
-        thrust::counting_iterator<int> c0(0);
-        thrust::counting_iterator<int> c1(sa.size());
-        thrust::device_vector<char>char_a ;
-
-        cout<<subseq_a[10];
-  
-        thrust::transform(c0, c1, calc[1].begin()+1, meu_functor( subseq_a.data(), subseq_b.data(), sa.data(), sb.data(), char_a.data()));   
-     
-
-        //  meu_functor(subseq_a, subseq_b, sa, sb)
-
     return 0;
 
 }
 
 
-
-
-    //             for(int i=0; i< int(sb_len.size()); i++){
-                    
-    //                thrust::transform(c0, c1, calc[1].begin()+1, meu_functor(d_S.data, sb[i], calc[0].data()));
-    //                thrust::inclusive_scan(calc[1].begin()+1, calc[1].end(), calc[0].begin()+1, thrust::maximum<int>());
-
-
-// aa
-// bbb
-
-
-
-// [a, aa]
-// [b, bb, bbb]
-
-
-
-// [0,0,0,1,1,1]
-// [0,1,2]
-
-// [a, aa, aaa]
-// [b, bb]
-
-// [a, a,aa, aa, aaa, aaa]
-// [b,bb,b,bb,b,bb]
--- Exaustiva_GPU/exaustiva_final.cu
+++ Exaustiva_GPU/exaustiva_final.cu
@@ -4,7 +4,7 @@
 #include <thrust/transform.h>
 #include <iostream>
 #include <thrust/iterator/counting_iterator.h>
-
+#include <omp.h>
 #include <iostream>
 #include <algorithm>
 #include <vector>
@@ -27,15 +27,15 @@ struct meu_functor
     __host__ __device__
     int operator()(const int& x) {
 
-        int val = (b==sa[x]?2:-1);
+        int val = (b==sa[x-1]?2:-1);
 
         int diagonal= calc[x-1]+ val;
         int delecao=calc[x]-1;
 
-        if(diagonal>=delecao && diagonal>0){
+        if(diagonal>delecao && diagonal>0){
             return diagonal;
         }
-        else if( delecao >=diagonal && delecao>0){
+        else if( delecao >diagonal && delecao>0){
             return delecao;
         }else{
             return 0;
@@ -45,6 +45,27 @@ struct meu_functor
 
     }
 };
+
+
+struct esquerda
+{
+    __host__ __device__
+    int operator()(const int& x, const int & y) {
+
+       if(y>=(x-1) && y>=0){
+           return y;
+       }else if((x-1)>y &&(x-1)>=0){
+           return (x-1);
+       }else{
+
+           return 0;
+       }
+
+    }
+};
+
+
+
 int main()
 {
     int N, M;
@@ -75,7 +96,7 @@ int main()
 
 
     for(int i=0; i< N; i++){
-        string s="-";
+        string s="";
         for(int j=i; j<N; j++){
 
             s+=a[j];
@@ -86,7 +107,7 @@ int main()
     int suba_size= subseq_a.size();
 
     for(int i=0; i< M; i++){
-        string s="-";
+        string s="";
         for(int j=i; j<M; j++){
 
             s+=b[j];
@@ -97,38 +118,37 @@ int main()
     int subb_size= subseq_b.size();
     thrust::device_vector<int> calc[2];
 
-    thrust::counting_iterator<int> c0(0);
+    thrust::counting_iterator<int> c0(1);
 
   
     thrust::device_vector<int> max_scores;
+
+    #pragma omp  for collapse(2)
     for(int i=0; i<subb_size; i++){
         for(int j=0; j<suba_size; j++){
+
 
             thrust::device_vector<char> sa(subseq_a[j].begin(), subseq_a[j].end());
             thrust::device_vector<char> sb(subseq_b[i].begin(), subseq_b[i].end());
             calc[0].resize(int(sa.size())+1);
             calc[1].resize(int(sa.size())+1); 
             thrust::fill(calc[0].begin(), calc[0].end(),0);
-            thrust::counting_iterator<int> c1(int(sa.size()));
+            thrust::counting_iterator<int> c1(int(sa.size()+1));
            
             for(int n=0; n<int(sb.size()); n++ ){
 
                 char b=sb[n];
 
-                thrust::transform(c0, c1, calc[1].begin()+1, meu_functor( b, sa.data(), calc[0].data()));   
-                thrust::inclusive_scan(calc[1].begin()+1, calc[1].end(), calc[0].begin()+1, thrust::maximum<int>());
-
-                
-            }
-            max_scores.push_back(calc[0].back());
-
-         
-           
+                thrust::transform(c0, c1, calc[1].begin()+1, meu_functor( b, sa.data(), calc[0].data()));
+                thrust::inclusive_scan(calc[1].begin()+1, calc[1].end(), calc[0].begin()+1, esquerda());
+             
+            }      
+            max_scores.push_back(calc[0].back());         
         }    
     }
 
     // thrust::device_vector<int>::iterator best_score= thrust::max_element(max_scores.begin(), max_scores.end() );
-    int  best_score= thrust::reduce(max_scores.begin(), max_scores.end(), 0, thrust::maximum<int>() );
+    int  best_score= thrust::reduce(max_scores.begin(), max_scores.end(), 0, thrust::maximum<int>());
     
     cout<<best_score<<endl;
     
